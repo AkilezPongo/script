3828964da3c326783c56c7195d219f8a4c401183
Akilez <caomcano@hotmail.com>
2017-10-13 23:52:00 -0500

    add a new line that return value if they are direrent or equal the two files, whithout differences

--- script2.sh
+++ script2.sh
@@ -13,7 +13,8 @@ do
 	echo "$line <p1.in > $line.out "
 	$line <p1.in > $line.out
 	echo "checando diferencias $line"
-	diff $line.out out1.txt > $line.res  #.res tiene comparacion
+#	diff $line.out out1.txt > $line.res  #.res tiene comparacion
+	diff -q <(sort $line.out | uniq) <(sort out1.txt | uniq) > $line.res
 done    
 
 for line in $(find . -name "*2.c.exe") ;
@@ -21,6 +22,9 @@ do
     echo "ejecutando $line"
     echo "$line <p1.in > $line.out "
     $line <p2.in > $line.out
+#   diff $line.out out2.txt > $line.res  #.res tiene comparacion
+    diff -q <(sort $line.out | uniq) <(sort out2.txt | uniq) > $line.res
+
 
 done 
 
@@ -29,6 +33,9 @@ do
     echo "ejecutando $line"
     echo "$line <p1.in > $line.out "
     $line <p3.in > $line.out
+#   diff $line.out out3.txt > $line.res  #.res tiene comparacion
+    diff -q <(sort $line.out | uniq) <(sort out3.txt | uniq) > $line.res
+
 
 done
 
@@ -37,6 +44,10 @@ do
     echo "ejecutando $line"
     echo "$line <p1.in > $line.out "
     $line <p4.in > $line.out
+#   diff $line.out out4.txt > $line.res  #.res tiene comparacion
+    diff -q <(sort $line.out | uniq) <(sort out4.txt | uniq) > $line.res
+
+
 done
 
 for line in $(find . -name "*5.c.exe") ;
@@ -44,6 +55,10 @@ do
     echo "ejecutando $line"
     echo "$line <p1.in > $line.out "
     $line <p5.in > $line.out
+#   diff $line.out out5.txt > $line.res  #.res tiene comparacion
+    diff -q <(sort $line.out | uniq) <(sort out5.txt | uniq) > $line.res
+
+
 done
 
 
